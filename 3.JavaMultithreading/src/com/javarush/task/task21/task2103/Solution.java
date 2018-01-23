0b80fb36b4057222d7b58ca101c64dc74a8182e8
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-01-23 07:24:18 +0300

    some test commit

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
@@ -9,6 +9,13 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+        //some code
+        //som more code
+        System.out.println("hello");
+        System.out.println("smth");
+    }
 
+    public int calculate(int a, int b) {
+        return a + b;
     }
 }
