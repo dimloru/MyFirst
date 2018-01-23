f3daea2fec71e226984da74ea1dedac07d2133e5
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-01-23 07:43:37 +0300

    cleared everything

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2103/Solution.java
@@ -9,13 +9,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        //some code
-        //som more code
-        System.out.println("hello");
-        System.out.println("smth");
-    }
 
-    public int calculate(int a, int b) {
-        return a + b;
     }
 }
