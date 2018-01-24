42d1d5b902f1934edcd536c90b812b4dc8f413a9
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-01-24 09:16:11 +0300

    2104 Fixed HashCode (correct behavior with Sets, checked null Strings)

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2104/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task21.task2104;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /* 
@@ -17,26 +18,35 @@ public class Solution {
     public boolean equals(Object n) {
 //        return n.first.equals(first) && n.last.equals(last);
         if (this == n) return true;
-        System.out.println(getClass() + " - " + n.getClass());
+        if (n == null) return false;
         if (getClass() != n.getClass()) return false;
+//        if(!n.getClass().getSimpleName().equals("Solution")) return false;
+
         Solution sol = (Solution) n;
+
         if (first != null ? !first.equals(sol.first) : sol.first != null) return false;
         if (last != null ? !last.equals(sol.last) : sol.last != null) return false;
         return true;
     }
 
     public int hashCode() {
-        return 31 * first.hashCode() + last.hashCode();
+        int result = 0;
+        result = first == null ? 0 : first.hashCode();
+        result += last == null ? 0 : last.hashCode();
+        return 31 * result;
     }
+
+
+
 
     public static void main(String[] args) {
         Set<Solution> s = new HashSet<>();
         s.add(new Solution("Donald", "Duck"));
-        System.out.println(s.contains(new Solution("Donald", "Duck")));
+//        System.out.println(s.contains(new Solution("Donald", "Duck")));
         ///
-        Solution sol1 = new Solution("D", "Duck");
-        Solution sol2 = new Solution("D", "Duck");
-        System.out.println(sol1.hashCode());
-        System.out.println(sol2.hashCode());
+        Solution sol1 = new Solution("D", null);
+        Solution sol2 = new Solution("D", null);
+        System.out.println(sol1.equals(sol2));
+
     }
 }
