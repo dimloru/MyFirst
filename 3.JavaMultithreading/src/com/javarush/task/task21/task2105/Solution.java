c8ec26caba94009b58f90f0d725e3728aef6d211
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-01-24 14:42:09 +0300

    2105 Final Version

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2105/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task21.task2105;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 /* 
@@ -14,11 +15,21 @@ public class Solution {
         this.last = last;
     }
 
+    @Override
     public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
         if (!(o instanceof Solution))
             return false;
         Solution n = (Solution) o;
-        return n.first.equals(first) && n.last.equals(last);
+
+        return Objects.equals(first, n.first) && Objects.equals(last, n.last);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(first, last);
     }
 
     public static void main(String[] args) {
