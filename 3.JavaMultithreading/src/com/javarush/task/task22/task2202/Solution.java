5ff664913c93543a539190b2dfee155f8b14f606
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-01-26 16:10:45 +0300

    lecture 07 finish

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -7,25 +7,26 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
         System.out.println(getPartOfString("Амиго и Диего лучшие друзья!"));
-        System.out.println(getPartOfString("Амиго и Диего"));
+//        System.out.println(getPartOfString("Амиго и Диего"));
+        System.out.println(getPartOfString("F"));
+
 
     }
 
     public static String getPartOfString(String string) {
-        try {
-            int beginIndex = string.indexOf(" ");
-            int endIndex = beginIndex;
-            for (int i = 0; i < 3; i++) {
-                endIndex = string.indexOf(" ", endIndex + 1);
-            }
-            int lastSpace;
-            if ((lastSpace = string.indexOf(" ", endIndex + 1)) != -1) {
-                return string.substring(beginIndex, lastSpace);
-            } else {
-                return string.substring(beginIndex);
-            }
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new TooShortStringException();
+        if (string == null) throw new TooShortStringException();
+
+        int beginIndex = string.indexOf(" ");
+        int endIndex = beginIndex;
+        for (int i = 0; i < 3; i++) {
+            endIndex = string.indexOf(" ", endIndex + 1);
+            if (endIndex == -1) throw new TooShortStringException();
+        }
+        int lastSpace;
+        if ((lastSpace = string.indexOf(" ", endIndex + 1)) != -1) {
+            return string.substring(beginIndex + 1, lastSpace);
+        } else {
+            return string.substring(beginIndex + 1);
         }
     }
 
---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task22.task2203;
+
+/* 
+Между табуляциями
+*/
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string == null || string.isEmpty()) throw new TooShortStringException();
+        String result;
+        try {
+            int beginIndex = string.indexOf("\t");
+            int endIndex = string.indexOf("\t", beginIndex + 1);
+            result = string.substring(beginIndex + 1, endIndex);
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new TooShortStringException();
+        }
+        return result;
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
+        System.out.println(getPartOfString(null));
+    }
+}
