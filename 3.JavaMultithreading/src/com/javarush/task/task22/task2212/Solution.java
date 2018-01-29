47f36e8d3fb13c288eb758b2d5428bfb2f0d13a6
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-01-29 16:26:43 +0300

    2212 release

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -6,13 +6,24 @@ package com.javarush.task.task22.task2212;
 public class Solution {
     public static boolean checkTelNumber(String telNumber) {
         if (telNumber == null || telNumber.length() < 10) return false;
-        if (telNumber.indexOf("(") > telNumber.indexOf(")")) return false;
-        if (telNumber.matches("\\+?\\d*\\(?\\d{3}\\)?\\d*")) return true;
+        if (telNumber.indexOf("(") > telNumber.indexOf(")") ||
+                (!telNumber.contains("(") && telNumber.contains(")"))) return false;
+
+        char[] tel = telNumber.toCharArray();
+        int digtsCount = 0;
+        for (char ch : tel) {
+            if (Character.isDigit(ch)) digtsCount++;
+        }
+
+        if ((tel[0] == '+' && digtsCount == 12) || digtsCount == 10) {
+            if (telNumber.matches("\\+?\\d*\\(?\\d{3}\\)?\\d*-?\\d+-?\\d+")) return true;
+        }
+
         return false;
     }
 
     public static void main(String[] args) {
-        System.out.println(checkTelNumber("+380501234567"));
+        System.out.println(checkTelNumber("(0)501234567"));
 
     }
 }
