c386c95584922e35fe0dbc06e74e95674e085350
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-01-30 10:25:04 +0300

    2312 Snake after 10

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Room.java
@@ -1,7 +1,61 @@
 package com.javarush.task.task23.task2312;
 
 public class Room {
+    private int width, height;
+    private Snake snake;
+    private Mouse mouse;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Snake getSnake() {
+        return snake;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
+    }
+
+    public void setSnake(Snake snake) {
+        this.snake = snake;
+    }
+
+    public Room(int width, int height, Snake snake) {
+        this.width = width;
+        this.height = height;
+        this.snake = snake;
+    }
+
+    public static Room game;
+
     public static void main(String[] args) {
+        game = new Room(100, 100, new Snake(50,50));
+        game.getSnake().setDirection(SnakeDirection.DOWN);
+
+    }
+
+    void run() {
+
+    }
+
+    void print() {
 
     }
 }
