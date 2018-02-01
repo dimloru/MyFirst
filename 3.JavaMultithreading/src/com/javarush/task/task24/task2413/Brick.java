f2bf2e02ca975c6ea89ffb8bbf31c9d2cfeb3d73
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-02-01 12:54:29 +0300

    2413 after 13

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -1,8 +1,8 @@
 package com.javarush.task.task24.task2413;
 
 public class Brick extends BaseObject {
-    public Brick(double x, double y, double radius) {
-        super(x, y, radius);
+    public Brick(double x, double y) {
+        super(x, y, 3);
     }
 
     @Override
@@ -12,6 +12,6 @@ public class Brick extends BaseObject {
 
     @Override
     void move() {
-
+        // A brick doesn't move
     }
 }
