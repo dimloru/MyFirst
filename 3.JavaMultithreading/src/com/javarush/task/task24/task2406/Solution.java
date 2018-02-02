64fe757bef0f26b42fa3a975f36f6d0bbf2be03b
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-02-02 14:10:18 +0300

    2405 in progress

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2406/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2406/Solution.java
@@ -19,6 +19,19 @@ public class Solution {
         }
     }
 
+    public class Apt3Bedroom extends Building.Apartments {
+        Apt3Bedroom(Building building) {
+            building.super();
+        }
+    }
+
+    public class BigHall extends Building.Hall {
+        BigHall(Building building, BigDecimal square) {
+            building.super(square);
+        }
+
+    }
+
     public static void main(String[] args) {
 
     }
