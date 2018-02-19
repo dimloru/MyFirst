a0a9452de95bbf97ba9c9b745d1e6e4778f91546
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-02-19 11:57:57 +0300

    3008 after 8

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,8 +3,14 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static com.javarush.task.task30.task3008.MessageType.*;
 
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
     public static void main(String[] args) {
 //        System.out.println("Введите номер порта");
         int port = ConsoleHelper.readInt();
@@ -26,6 +32,36 @@ public class Server {
 
         public Handler (Socket socket) {
             this.socket = socket;
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            connection.send(new Message(NAME_REQUEST));
+            Message response = connection.receive();
+            String name = response.getData();
+
+            while (response.getType() != USER_NAME ||
+                    name.isEmpty() ||
+                    connectionMap.containsKey(name)) {
+                connection.send(new Message(NAME_REQUEST));
+                response = connection.receive();
+                name = response.getData();
+            }
+
+            connectionMap.put(name, connection);
+            connection.send(new Message(NAME_ACCEPTED));
+
+            return name;
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (String name : connectionMap.keySet()) {
+            try {
+                connectionMap.get(name).send(message);
+            } catch (IOException e) {
+                System.out.printf("Could not sent message to %s", name);
+//                e.printStackTrace();
+            }
         }
     }
 }
