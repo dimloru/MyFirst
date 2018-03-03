80ee39b0cd7435d6e1159be74303df085b4ddeff
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-03-03 11:41:51 +0300

    3110 Archiver. After 4

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -26,5 +28,7 @@ public class Archiver {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        new ExitCommand().execute();
     }
 }
