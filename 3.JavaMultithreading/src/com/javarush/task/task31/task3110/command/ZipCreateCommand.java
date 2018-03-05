dd50130d11d6a5a05d53b3371f6a43f95df90ba4
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-03-05 18:02:36 +0300

    3110 Archiver. After 12

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
@@ -7,23 +7,22 @@ import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static com.javarush.task.task31.task3110.ConsoleHelper.readString;
-
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         try {
             ConsoleHelper.writeMessage("Создание архива.");
+
             ZipFileManager zipFileManager = getZipFileManager();
-            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации");
-            String sourcePathString = ConsoleHelper.readString();
-            Path sourcePath = Paths.get(sourcePathString);
+
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
             zipFileManager.createZip(sourcePath);
+
             ConsoleHelper.writeMessage("Архив создан.");
+
         } catch (PathIsNotFoundException e) {
             ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
         }
-
-
     }
 }
