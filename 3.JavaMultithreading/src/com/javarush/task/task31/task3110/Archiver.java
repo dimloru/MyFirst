6cb5c17df610796ea2c0d4232b0a5c74f98d65b6
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-03-05 14:28:15 +0300

    3110 Archiver. After 9 FileManager Class added

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,20 +1,14 @@
 package com.javarush.task.task31.task3110;
 
-import com.javarush.task.task31.task3110.command.Command;
-import com.javarush.task.task31.task3110.command.ExitCommand;
 import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) {
-        Operation operation = null;
+    public static void main(String[] args) throws IOException {
 
-        while (true) {
+        Operation operation = null;
+        do {
             try {
                 operation = askOperation();
                 CommandExecutor.execute(operation);
@@ -24,54 +18,20 @@ public class Archiver {
                 ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
             }
 
-            if (operation == Operation.EXIT) break;
-
-//            String zipPathString = null, sourcePathString = null;
-//
-//            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-//                System.out.println("Please enter the path to the archive:");
-//                zipPathString = reader.readLine();
-//                System.out.println("Please enter the path to the source file:");
-//                sourcePathString = reader.readLine();
-//            } catch (IOException e) {
-//            }
-//
-//            Path zipPath = Paths.get(zipPathString);
-//            ZipFileManager zipFileManager = new ZipFileManager(zipPath);
-//            Path sourcePath = Paths.get(sourcePathString);
-//
-//            try {
-//                zipFileManager.createZip(sourcePath);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-//            try {
-//                new ExitCommand().execute();
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-        }
+        } while (operation != Operation.EXIT);
     }
 
+
     public static Operation askOperation() throws IOException {
-        int operationNumber = -1;
-        while (true) {
-            ConsoleHelper.writeMessage("Выберите операцию:");
-            ConsoleHelper.writeMessage("(" + Operation.CREATE.ordinal() + ") - упаковать файлы в архив");
-            ConsoleHelper.writeMessage("(" + Operation.ADD.ordinal() + ") - добавить файл в архив");
-            ConsoleHelper.writeMessage("(" + Operation.REMOVE.ordinal() + ") - удалить файл из архива");
-            ConsoleHelper.writeMessage("(" + Operation.EXTRACT.ordinal() + ") - распаковать архив");
-            ConsoleHelper.writeMessage("(" + Operation.CONTENT.ordinal() + ") - просмотреть содержимое архива");
-            ConsoleHelper.writeMessage("(" + Operation.EXIT.ordinal() + ") - выход");
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
 
-            operationNumber = ConsoleHelper.readInt();
-
-            if (operationNumber > 5 || operationNumber < 0)
-                ConsoleHelper.writeMessage("Неподдрживаемая операция, попробуйте еще раз");
-            else break;
-        }
-
-        return Operation.values()[operationNumber];
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
