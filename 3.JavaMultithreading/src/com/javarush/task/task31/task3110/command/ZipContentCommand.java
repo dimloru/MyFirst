586bd90a4d547b481f6f537063118b8ce3350f2b
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-03-06 13:27:28 +0300

    3110 Archiver. After 15

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -1,21 +1,25 @@
 package com.javarush.task.task31.task3110.command;
 
-
 import com.javarush.task.task31.task3110.ConsoleHelper;
 import com.javarush.task.task31.task3110.FileProperties;
 import com.javarush.task.task31.task3110.ZipFileManager;
 
-import java.nio.file.Paths;
 import java.util.List;
 
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-//        ConsoleHelper.writeMessage("Введите имя архива:");
-//        String zipName = ConsoleHelper.readString();
-//        ZipFileManager manager = new ZipFileManager(Paths.get(zipName));
-//        List<FileProperties> list =  manager.getFilesList();
-//        for (FileProperties property : list) ConsoleHelper.writeMessage(property.toString());
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
 
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
+        List<FileProperties> files = zipFileManager.getFilesList();
+        for (FileProperties file : files) {
+            ConsoleHelper.writeMessage(file.toString());
+        }
+
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
