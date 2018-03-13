eede47c86b192a38c6ce77e177d3302af0643290
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-03-13 17:32:22 +0300

    3209 after 4

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -1,11 +1,17 @@
 package com.javarush.task.task32.task3209;
 
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
+
+    private JTabbedPane tabbedPane = new JTabbedPane();
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
 
     public Controller getController() {
         return controller;
@@ -21,7 +27,24 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void init() {
+        initGui();
+        addWindowListener(new FrameListener(this));
+        setVisible(true);
 
+    }
+
+    public void initMenuBar() {
+
+    }
+
+    public void initEditor() {
+
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
     }
 
     public void exit() {
