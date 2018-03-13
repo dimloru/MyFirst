b5160acc0af8308dc0b16f37cb0308c096bf6973
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-03-13 17:02:31 +0300

    3209

---
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task32.task3209;
+
+import javax.swing.text.html.HTMLDocument;
+import java.io.File;
+
+public class Controller {
+    private View view;
+    private HTMLDocument document;
+    private File currentFile;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public static void main(String[] args) {
+        View myView = new View();
+        Controller controller = new Controller(myView);
+        myView.setController(controller);
+        myView.init();
+        controller.init();
+
+
+    }
+
+    public void init() {
+
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
+
+
+}
