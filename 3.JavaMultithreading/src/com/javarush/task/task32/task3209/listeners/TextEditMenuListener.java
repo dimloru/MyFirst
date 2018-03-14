ff0b62ad3f17f5fc4ae1624434e165ae86caa511
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-03-14 19:37:19 +0300

    3209 after 13

--- 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
+++ 3.JavaMultithreading/src/com/javarush/task/task32/task3209/listeners/TextEditMenuListener.java
@@ -2,17 +2,30 @@ package com.javarush.task.task32.task3209.listeners;
 
 import com.javarush.task.task32.task3209.View;
 
+import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
+import java.awt.*;
 
 public class TextEditMenuListener implements MenuListener {
-    public TextEditMenuListener(View view) {
+    private View view;
 
+    public TextEditMenuListener(View view) {
+        this.view = view;
     }
 
     @Override
-    public void menuSelected(MenuEvent e) {
+    public void menuSelected(MenuEvent menuEvent) {
+        JMenu selected = (JMenu)menuEvent.getSource();
 
+        Component[] components = selected.getMenuComponents();
+
+        for (Component component : components) {
+            if (view.isHtmlTabSelected()) component.setEnabled(true);
+            else {
+                component.setEnabled(false);
+            }
+        }
     }
 
     @Override
