58b91a5c2ad5a8603ac911b19a217a97392998f2
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-03-20 18:45:57 +0300

    pre 2712 Resturant 10 recursion pre-check

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -1,26 +1,40 @@
 package com.javarush.task.task27.task2712.ad;
 
+
+
 import com.javarush.task.task27.task2712.ConsoleHelper;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
     int timeSeconds; // cooking time in seconds
+//    private List<List<Advertisement>> adVariants = new ArrayList<>();
+    private List<Advertisement> toShow = new ArrayList<>();
 
     public AdvertisementManager(int timeSeconds) {
         this.timeSeconds = timeSeconds;
     }
 
     public void processVideos() {
-//        List<Advertisement> toShow = new ArrayList<>();
-//        toShow.add(storage.list().get(0));
-
         if (storage.list().isEmpty()) {
             throw new NoVideoAvailableException();
         }
+
+        searchAdVariants(0, null, 0); //можно потом перенести и сделать return
+
+        if (toShow.isEmpty()) {
+            throw new NoVideoAvailableException();
+        }
+
+        for (Advertisement ad : toShow) {
+            ConsoleHelper.writeMessage(ad.getName() + " is displaying... " + ad.getAmountPerOneDisplaying() + ", " +
+                    + (int)(ad.getOneSecCost() * 1000));   //secCost - to Advert
+        }
+
+
+
 
 //        Collections.sort(toShow, (p1, p2) -> {
 //            long amountPerOneDisplayingP1 = p1.getAmountPerOneDisplaying();
@@ -36,14 +50,65 @@ public class AdvertisementManager {
 //            return Double.compare(p1.getOneSecCost(), p2.getOneSecCost());
 //        });
 
-//        for (Advertisement ad : toShow) {
-//          ConsoleHelper
-//            System.out.println(ad.getName() + " is displaying... " + ad.getAmountPerOneDisplaying() + ", " +
-//                    + (int)(ad.getOneSecCost() * 1000));   //secCost - to Advert
-//
-//        }
+
 
 
 
     }
+
+    private void searchAdVariants (int startFrom, List<Advertisement> inList, int inTime) {
+        if (inList == null) inList = new ArrayList<>();
+        int maxMoney = 0;
+
+        for (int i = startFrom; i < storage.list().size(); i++) {
+            Advertisement element = storage.list().get(i);
+
+            if ((inTime + element.getDuration()) <= timeSeconds) { // если элемент можо добавить по времени
+                int outTime = inTime + element.getDuration();
+                List<Advertisement> outList = new ArrayList<>(inList);
+                outList.add(element);
+                if (toShow.isEmpty()) toShow = outList;
+
+                int thisMoney = 0;
+                int toShowMoney = 0;
+                for (Advertisement ad : outList) {
+                    thisMoney += ad.getAmountPerOneDisplaying();
+                }
+                for (Advertisement ad : toShow) {
+                    toShowMoney += ad.getAmountPerOneDisplaying();
+                }
+
+                if (thisMoney == toShowMoney) {
+                    int thisTime = 0;
+                    int toShowTime = 0;
+                    for (Advertisement ad : outList) {
+                        thisTime += ad.getDuration();
+                    }
+                    for (Advertisement ad : toShow) {
+                        toShowTime += ad.getDuration();
+                    }
+
+                    if (thisTime > toShowTime) {
+                        toShow = outList;
+                    } else if (thisTime == toShowTime) {
+                        if (outList.size() < toShow.size()){
+                            toShow = outList;
+                        }
+                    }
+
+//                    adVariants.add(outList);
+
+                } else if (thisMoney > toShowMoney) {
+                    toShow = outList;
+//                    adVariants.clear();
+//                    adVariants.add(outList);
+                    maxMoney = thisMoney;
+                }
+
+                if (i < storage.list().size()) searchAdVariants(i + 1, outList, outTime);
+            }
+        }
+    }
+
+
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementStorage.java
@@ -12,9 +12,9 @@ public class AdvertisementStorage {
 
     private AdvertisementStorage() {
         Object someContent = new Object();
-        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min
-        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min
-        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60)); //10 min
+        videos.add(new Advertisement(someContent, "First Video", 5000, 100, 3 * 60)); // 3 min   //50
+        videos.add(new Advertisement(someContent, "Second Video", 100, 10, 15 * 60)); //15 min   //10
+        videos.add(new Advertisement(someContent, "Third Video", 400, 2, 10 * 60)); //10 min     //200
     }
 
     private final List<Advertisement> videos = new ArrayList<>();
