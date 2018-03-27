d055659a8bcb794b760a25bc77b78e1e05a41243
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-03-27 13:34:56 +0300

    3513 after 6

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -15,6 +15,7 @@ public class Model {
 
     public void resetGameTiles() {
         score = 0;
+        maxTile = 2;
         gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
         for (int i = 0; i < FIELD_WIDTH; i++) {
             for (int j = 0; j < FIELD_WIDTH; j++) {
@@ -23,11 +24,10 @@ public class Model {
         }
         addTile();
         addTile();
-        maxTile = 2;
     }
 
 
-    public List<Tile> getEmptyTiles() {
+    private List<Tile> getEmptyTiles() {
         List<Tile> result = new ArrayList<>();
         for (int i = 0; i < FIELD_WIDTH; i++) {
             for (int j = 0; j < FIELD_WIDTH; j++) {
@@ -39,38 +39,57 @@ public class Model {
 
     private void addTile() {
         List<Tile> list = getEmptyTiles();
-        int num = (int) (Math.random() * list.size());
-        if (num == list.size()) num--;
-        list.get(num).setValue(Math.random() < 0.9 ? 2 : 4);
+        if (!list.isEmpty()) {
+            int num = (int) (Math.random() * list.size());
+            if (num == list.size()) num--;
+            list.get(num).setValue(Math.random() < 0.9 ? 2 : 4);
+        }
     }
 
-    private Tile[] compressTiles(Tile[] tiles) {
-        for (int i = 0; i < tiles.length; i++) {
-            if (tiles[i].getValue() == 0) {
-                for (int j = i; j < tiles.length; j++) {
-                    if (tiles[j].getValue() != 0) {
-                        tiles[i].setValue(tiles[j].getValue());
-                        tiles[j].setValue(0);
-                        break;
+    private boolean compressTiles(Tile[] tiles) {
+        boolean changed = false;
+        if (tiles != null) {
+            for (int i = 0; i < tiles.length; i++) {
+                if (tiles[i].getValue() == 0) {
+                    for (int j = i; j < tiles.length; j++) {
+                        if (tiles[j].getValue() != 0) {
+                            tiles[i].setValue(tiles[j].getValue());
+                            tiles[j].setValue(0);
+                            changed = true;
+                            break;
+                        }
                     }
                 }
+                if (tiles[i].getValue() == 0) break;
             }
-            if (tiles[i].getValue() == 0) break;
         }
-        return tiles;
+        return changed;
     }
 
-    private Tile[] mergeTiles(Tile[] tiles) {
-        for (int i = 0; i < tiles.length - 1; i++) {
-            if (tiles[i].value == tiles [i + 1].value && tiles[i].value != 0) {
-                tiles[i].value *= 2;
-                tiles[i + 1].value = 0;
-                score += tiles[i].value;
-                if (tiles[i].value > maxTile) maxTile = tiles[i].value;
+    private boolean mergeTiles(Tile[] tiles) {
+        boolean changed = false;
+        if (tiles != null) {
+            for (int i = 0; i < tiles.length - 1; i++) {
+                if (tiles[i].value == tiles[i + 1].value && tiles[i].value != 0) {
+                    tiles[i].value *= 2;
+                    tiles[i + 1].value = 0;
+                    score += tiles[i].value;
+                    if (tiles[i].value > maxTile) maxTile = tiles[i].value;
+                    changed = true;
+                }
             }
+            compressTiles(tiles);
         }
-        tiles = compressTiles(tiles);
-        return tiles;
+        return changed;
+    }
+
+    public void left() {
+        boolean changed = false;
+        for (int i = 0; i < gameTiles.length; i++) {
+            if (compressTiles(gameTiles[i])) changed = true;
+            if (mergeTiles(gameTiles[i])) changed = true;
+        }
+        if (changed) addTile();
     }
 
 }
