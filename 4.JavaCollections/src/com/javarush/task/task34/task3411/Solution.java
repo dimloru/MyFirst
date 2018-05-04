2a3cfeb4230b7b377d104d5ad68ef93b60bb0368
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-05-04 16:38:13 +0300

    3411 Hanoi tower

--- 4.JavaCollections/src/com/javarush/task/task34/task3411/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3411/Solution.java
@@ -12,5 +12,30 @@ public class Solution {
 
     public static void moveRing(char a, char b, char c, int count) {
         //напишите тут ваш код
+//        if (count == 1) System.out.println("from " + a + " to " + b);
+//        else if (count == 2) {
+//            System.out.println("from " + a + " to " + c);
+//            System.out.println("from " + a + " to " + b);
+//            System.out.println("from " + c + " to " + b);
+//        }
+//        else {
+//            if (count % 2 == 0){ // even
+//                moveRing(a, c, b, (count - 1));
+//                System.out.println("from " + a + " to " + b);
+//                moveRing(c, b, a, (count - 1));
+//            }
+//            else { // odd
+//                moveRing(a, c, b, (count - 1));
+//                System.out.println("from " + a + " to " + b);
+//                moveRing(c, b, a, (count - 1));
+//            }
+//        }
+
+        // second edition
+        if (count > 0) {
+            moveRing(a, c, b, count - 1);
+            System.out.println("from " + a + " to " + b);
+            moveRing(c, b, a, count - 1);
+        }
     }
 }
