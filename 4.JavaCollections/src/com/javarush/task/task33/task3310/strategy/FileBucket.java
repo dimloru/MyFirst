0ec90d68c6764302922c00a9f0b8079edef559ec
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-05-16 15:12:29 +0300

    3310 after 9

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/FileBucket.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task33.task3310.strategy;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileBucket {
+    private Path path;
+    private File file;
+
+    public FileBucket() {
+        try {
+            path = Files.createTempFile("","");
+            Files.deleteIfExists(path);
+            Files.createFile(path);
+            file = path.toFile();
+            file.deleteOnExit();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public long getFileSize() {
+        long size = 0;
+        try {
+            size = Files.size(path);
+        } catch (IOException e) {e.printStackTrace();}
+        return size;
+    }
+
+    public void putEntry(Entry entry) {
+        try (
+            OutputStream fos = Files.newOutputStream(path);
+            ObjectOutputStream oos = new ObjectOutputStream(fos)
+        ) {
+            oos.writeObject(entry);
+        } catch (IOException e) { e.printStackTrace(); }
+    }
+
+    public Entry getEntry() {
+        Entry entry = null;
+        if (getFileSize() > 0) {
+            try (
+                    InputStream fis = Files.newInputStream(path);
+                    ObjectInputStream ois = new ObjectInputStream(fis)
+            ) {
+                entry = (Entry)ois.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return entry;
+    }
+
+    public void remove() {
+        try {
+            Files.delete(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
