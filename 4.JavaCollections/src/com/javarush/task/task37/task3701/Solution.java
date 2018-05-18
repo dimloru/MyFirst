d7016dd1461ce257d89332f99db8eaf4065ed0a6
Dmitry Loshchenkov <dimlo@yandex.ru>
2018-05-18 19:18:38 +0300

    3703

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3701/Solution.java
@@ -0,0 +1,78 @@
+package com.javarush.task.task37.task3701;
+
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+/* 
+Круговой итератор
+*/
+public class Solution<T> extends ArrayList<T> {
+    public static void main(String[] args) {
+        Solution<Integer> list = new Solution<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        int count = 0;
+        for (Integer i : list) {
+            //1 2 3 1 2 3 1 2 3 1
+            System.out.print(i + " ");
+            count++;
+            if (count == 10) {
+                break;
+            }
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new RoundIterator();
+    }
+
+    public class RoundIterator implements Iterator<T>{
+        int cursor = 0;
+        int lastRet = -1;
+        int expectedModCount = modCount;
+
+        @Override
+        public boolean hasNext() {
+            return size() != 0;
+        }
+
+        @Override
+        public T next() {
+            checkForComodification();
+            int i = cursor;
+            if (i >= size())
+                i = 0;
+            Object[] elementData = Solution.this.toArray(); //??
+            if (i >= elementData.length)
+                throw new ConcurrentModificationException();
+            cursor = i + 1;
+            return (T) elementData[lastRet = i];
+        }
+
+        @Override
+        public void remove() {
+            if (lastRet < 0)
+                throw new IllegalStateException();
+            checkForComodification();
+
+            try {
+                Solution.this.remove(lastRet);
+                cursor = lastRet;
+                lastRet = -1;
+                expectedModCount = modCount;
+            } catch (IndexOutOfBoundsException ex) {
+                throw new ConcurrentModificationException();
+            }
+
+        }
+
+        final void checkForComodification() {
+            if (modCount != expectedModCount)
+                throw new ConcurrentModificationException();
+        }
+    }
+}
